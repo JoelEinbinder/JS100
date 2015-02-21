68750ab26e6d2ae7a102e8b2d0a3492c0dbac8c1
hrlarry <hrlarry@stanford.edu>
2015-02-21 06:41:41 -0800

    started drawing header for demo app

--- demo/dummyHome.js
+++ demo/dummyHome.js
@@ -45,10 +45,105 @@ function drawMoreMenu(){
 */
 function drawHeader(text, activeMenu){
 
-	root.addSubview(new View({
-		
+	drawTitle(text);
+	drawNavBar(activeMenu);
+
+}
+
+function drawTitle(text){
+	//Wrap menu title, search button, and friend button
+	var titleWrapper = new View({
+		color:blue,
+		height:30,
+		scalar:{
+			width: 1,
+		}
+	})
+	root.addSubview(titleWrapper);
+	
+	//Add menu title
+	titleWrapper.addSubview(new TextView({
+		metrics: {
+			color:white,
+			x: 6,
+			y: 6,
+			scalar: {
+				width: 0.7,
+				height: 0.6
+			}
+		},
+		text: text
 	}));
 
+	//Add search button
+	titleWrapper.addSubview(new ImageView({
+		metrics:{
+			y: 6,
+			scalar:{
+				x: .733
+				width: 0.1,
+				height: 0.6
+			}
+		},
+		imagepath: "http://iconizer.net/files/IconSweets_2/orig/search_2.png" //TODO: Correct image path property for search icon
+	}));
+
+	//Add friend button
+	titleWrapper.addSubview(new ImageView({
+		metrics:{
+			y:6,
+			scalar:{
+				x: .867
+				width: 0.1,
+				height: 0.6
+			}
+		},
+		imagepath: "http://cdns2.freepik.com/free-photo/menu-circle_318-26611.jpg" //TODO: Correct image path property for friend icon
+	}));
+}
+
+function drawNavBar(activeMenu){
+	var navBarWrapper = new View({
+		color:blue,
+		y:30,
+		height:30,
+		scalar:{
+			width: 1,
+		}
+	})
+	root.addSubview(navBarWrapper);
+
+	for(var i = 0; i < 5; i++){
+		var currNavItem = drawNavItem(i, activeMenu == i, "http://iconizer.net/files/IconSweets_2/orig/search_2.png");
+		navBarWrapper.addSubview(currNavItem);
+		if(pushNum[i] != 0){ //TODO: Figure out how to retrieve this data
+			//Need to display push notifications for this
+			drawPushBubble(pushNum);
+		}
+	}
+}
+
+/**
+* Draws a single nav bar item
+* @index: this item is the index-th nav bar item. Used for positioning
+* @activeStatus: determines if this nav bar item is currently active
+* @pushNum: number of push notifications to display in alert
+*/
+function drawNavItem(index, activeStatus, imagepathname){
+
+	//TODO: account for activeStatus
+
+	var navItem = new ImageView({
+		metrics:{
+			scalar:{
+				x: index * 0.2 + 0.05, //Each tab occupies 20% of width, but icon occupies central 10% within that 20% frame
+				width: 0.1
+			}
+		},
+		imagepath: imagepathname
+	});
+
+	return navItem;
 }
 
 /*
