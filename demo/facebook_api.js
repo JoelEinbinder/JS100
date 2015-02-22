3281afe07c047a18ad90c80c3bd4ca6f0a740d10
hrlarry <hrlarry@stanford.edu>
2015-02-21 22:32:20 -0800

    dropped /me endpoint

--- demo/facebook_api.js
+++ demo/facebook_api.js
@@ -76,17 +76,16 @@ FB.getLoginStatus(function(response) {
 // successful.  See statusChangeCallback() for when this call is made.
 function testAPI() {
   console.log('Welcome!  Fetching your information.... ');
-  FB.api('/me', function(response) {
+  /*FB.api('/me', function(response) {
     console.log('Successful login for: ' + response.name);
     console.log("Profile object");
     console.log(response);
     document.getElementById('status').innerHTML =
       'Thanks for logging in, ' + response.name + '!';
-    setTimeout(function(){
-        getNotifications();
+    getNotifications();
 
-      },1000);
-  });
+  });*/
+  getNotifications();
 
   
 }
