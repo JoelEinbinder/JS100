dafeb2a7af944f72ed67db681728ce584ff54b70
hrlarry <hrlarry@stanford.edu>
2015-02-21 21:39:07 -0800

    debugging sdk connection issues

--- demo/facebook_api.js
+++ demo/facebook_api.js
@@ -9,7 +9,6 @@ function statusChangeCallback(response) {
   if (response.status === 'connected') {
     // Logged into your app and Facebook.
     testAPI();
-    addPermissions();
   } else if (response.status === 'not_authorized') {
     // The person is logged into Facebook, but not your app.
     document.getElementById('status').innerHTML = 'Please log ' +
@@ -77,7 +76,8 @@ function testAPI() {
   console.log('Welcome!  Fetching your information.... ');
   FB.api('/me', function(response) {
     console.log('Successful login for: ' + response.name);
-    console.log("Access token: " + response.authResponse.accessToken);
+    addPermissions();
+    //console.log("Access token: " + response.authResponse.accessToken);
     document.getElementById('status').innerHTML =
       'Thanks for logging in, ' + response.name + '!';
   });
